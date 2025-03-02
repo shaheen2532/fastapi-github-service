2fae682156c41551b9fa08bab580a0802426ad3b
MUHAMMED SHAHEEN <shaheen2532@gmail.com>
2025-03-02 06:02:04 +0400

    slowapi limiter configured

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from slowapi import Limiter
+from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.middleware import SlowAPIMiddleware
+from slowapi.errors import RateLimitExceeded
 from router import router
 from logger import logger
 import os
@@ -10,11 +11,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-limiter = Limiter(key_func=get_remote_address)
+limiter = Limiter(key_func=get_remote_address, default_limits=["5/minute"])
 PORT = os.getenv("PORT")
 
-app = FastAPI()
+app = FastAPI(title="FastAPI Paginated API Service", version="1.0")
 app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(SlowAPIMiddleware)
 app.add_middleware(
     CORSMiddleware,
@@ -23,9 +25,11 @@ app.add_middleware(
     allow_methods=["GET"],
     allow_headers=["X-API-KEY"],
 )
+
 app.include_router(router)
 
 @app.get("/")
+@limiter.exempt
 def health_check():
     logger.info("Server is running on port: "+ str(PORT))
     return {"status" : "FastAPI service is running", "port": PORT}
